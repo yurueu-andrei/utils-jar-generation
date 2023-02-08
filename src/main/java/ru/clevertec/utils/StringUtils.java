71ebd3cc21c82b276baf7a53e8b3212dda495df7
Dobrowydka <andrikchannelyou@gmail.com>
2023-02-08 17:01:10 +0300

    feat: create own StringUtils class

---
+++ src/main/java/ru/clevertec/utils/StringUtils.java
@@ -0,0 +1,7 @@
+package ru.clevertec.utils;
+
+public class StringUtils {
+    public static boolean isPositiveNumber(String str) {
+        return org.apache.commons.lang3.StringUtils.isNumeric(str);
+    }
+}
